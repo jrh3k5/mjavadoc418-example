0c4cc45b3372d5eec009c3f2bf1fcaf8e30c48bb
Joshua Hyde <joshua.hyde@cerner.com>
2015-10-05 13:07:28 -0500

    Add project

---
+++ src/test/java/com/github/jrh3k5/example/FooTest.java
@@ -0,0 +1,13 @@
+package com.github.jrh3k5.example;
+
+import org.junit.Test;
+
+public class FooTest {
+	/**
+	 * Test javadoc linking for {@link Foo}.
+	 */
+	@Test
+	public void testSomething() {
+		new Foo();
+	}
+}
